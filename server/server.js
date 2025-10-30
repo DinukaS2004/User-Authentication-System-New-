d38c1588c3dfcaca3edc4c2084049d23c022d53b
Dinuka Samarasinghe <dinukad735@gmail.com>
2025-10-30 18:21:50 +0530

    created user routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config.js';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from './routes/authRoutes.js'
+import userRouter from "./routes/userRoutes.js";
 
 
 const app = express();
@@ -18,7 +19,8 @@ app.use(cors({credentials:true}));
 
 // API Endpoints
 app.get('/',(req,res)=> res.send("API Working fine"));
-app.use('/api/auth',authRouter)
+app.use('/api/auth',authRouter);
+app.use('/api/user',userRouter);
 
 app.listen(port,() => console.log(`Server started on PORT : ${port}`));
 
