0adce2a0b69df466c844ea517bcc01d98866fa81
Dinuka Samarasinghe <dinukad735@gmail.com>
2025-10-30 20:05:30 +0530

    Fixed a bug

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ export const getUserData = async (req,res) =>{
     try {
         const {userId} = req.body;
 
-        const user = await usesrModel.findById(userId);
+        const user = await userModel.findById(userId);
 
         if(!user){
             return res.json({success:false, message:'User not found'});
@@ -17,7 +17,7 @@ export const getUserData = async (req,res) =>{
                 isAccountVerified:user.isAccountVerified
             }
         });
-        
+
     } catch (error) {
         res.json({success:false,message:error.message});
     }
