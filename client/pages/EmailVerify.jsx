d96f0fb94dfed78c0635b42667ad69a160cc8ff1
Dinuka Samarasinghe <dinukad735@gmail.com>
2025-11-05 09:25:43 +0530

    fixed all issues

---
+++ client/pages/EmailVerify.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const EmailVerify = () => {
+  return (
+    <div>
+        <h1>EmailVerify</h1>
+    </div>
+  )
+}
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import {Routes,Router, Route} from 'react-router-dom'
+import { Home } from '../pages/Home'
+import { Login } from '../pages/Login'
+import { EmailVerify } from '../pages/EmailVerify'
+import { ResetPassword } from '../pages/ResetPassword'
 
-export const App = () => {
+const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path = '/' element = {<Home/>}/>
+        <Route path = '/login' element = {<Login/>}/>
+        <Route path = '/email-verify' element = {<EmailVerify/>}/>
+        <Route path = '/reset-password' element = {<ResetPassword/>}/>
+      </Routes>
+    </div>
   )
 }
+export default App;
