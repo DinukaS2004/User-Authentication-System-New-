b1fc9433e78b72cfe5d5c2c8d914726201fac495
Dinuka Samarasinghe <dinukad735@gmail.com>
2025-12-04 08:24:53 +0530

    finalized hte login page

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -1,16 +1,52 @@
 import React,{use, useContext, useState} from 'react'
 import { assets } from '../src/assets/assets'
 import { useNavigate } from 'react-router-dom';
+import { AppContent } from '../context/AppContext';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 export const Login = () => {
 
     const navigate = useNavigate();
-    const {backendURL} = useContext(AppContent);
+    const {backendURL,setIsloggedIn} = useContext(AppContent);
+    
     const [state,setState] = useState('Sign Up')
     const [name,setName] = useState('');
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
     
+    const onSubmitHandler = async(e) => {
+        try{
+            e.preventDefault();
+            
+            axios.defaults.withCredentials = true;
+
+            if(state === 'Sign Up'){
+                const {data} = await axios.post(backendURL+ '/api/auth/register',{name, email, password});
+
+                if(data.success){
+                    setIsloggedIn(true);
+                    navigate('/');
+
+                }else{
+                    toast.error(data.message);
+                }
+            }else{
+                const {data} = await axios.post(backendURL+ '/api/auth/login',{email, password});
+
+                if(data.success){
+                    setIsloggedIn(true);
+                    navigate('/');
+
+                }else{
+                    toast.error(data.message);
+                }
+            }
+        }catch(error){
+            toast.error(error.message);
+        }
+    }
+
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
         <img src={assets.logo} onClick={() => navigate('/')} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
@@ -18,7 +54,7 @@ export const Login = () => {
             <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up'? 'Create account':'Login'}</h2>
             <p className='text-center text-sm mb-6'>{state === 'Sign Up'? 'Create your account':'Login to  your account'}</p>
 
-            <form>
+            <form onSubmit={onSubmitHandler}>
                 {state === 'Sign Up' && (<div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
                     <img src={assets.person_icon} alt="" />
                     <input onChange={e=>setName(e.target.value)} value ={name} className='bg-transparent outline-none' type="text" placeholder='Full Name' required />
